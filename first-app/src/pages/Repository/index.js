b180594c98502037495c24aba7eb421a525748b2
Samuel Monteiro <samu.mjs@gmail.com>
2019-11-17 15:54:22 -0300

    Listando repositorios e issues

--- first-app/src/pages/Repository/index.js
+++ first-app/src/pages/Repository/index.js
@@ -1,17 +1,17 @@
+/* eslint-disable react/prop-types */
+import { Link } from 'react-router-dom';
 import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
 
 import api from '../../services/api';
 
-// import { Container } from './styles';
+import Container from '../../components/Container';
+import { Loading, Owner, IssueList } from './styles';
 
-export default function Repository({ match }) {
+function Repository({ match }) {
   const [repository, setRepository] = useState({});
   const [issues, setIssues] = useState([]);
   const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    repositoryData();
-  }, []);
 
   async function repositoryData() {
     const repoName = decodeURIComponent(match.params.repository);
@@ -31,5 +31,49 @@ export default function Repository({ match }) {
     setLoading(false);
   }
 
-  return <h1>Repository:</h1>;
+  useEffect(() => {
+    repositoryData();
+  }, []);
+
+  if (loading) {
+    return <Loading>Carregando...</Loading>;
+  }
+
+  return (
+    <Container>
+      <Owner>
+        <Link to="/">Voltar aos repositórios</Link>
+        <img src={repository.owner.avatar_url} alt={repository.owner.login} />
+        <h1>{repository.name}</h1>
+        <p>{repository.description}</p>
+      </Owner>
+
+      <IssueList>
+        {issues.map(issue => (
+          <li key={String(issue.id)}>
+            <img src={issue.user.avatar_url} alt={issue.user.login} />
+            <div>
+              <strong>
+                <a href={issue.html_url}>{issue.title}</a>
+                {issue.labels.map(label => (
+                  <span key={String(label.id)}>{label.name}</span>
+                ))}
+              </strong>
+              <p>{issue.user.login}</p>
+            </div>
+          </li>
+        ))}
+      </IssueList>
+    </Container>
+  );
 }
+
+Repository.prototype = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      repository: PropTypes.string,
+    }),
+  }).isRequired,
+};
+
+export default Repository;
